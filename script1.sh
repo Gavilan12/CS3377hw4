d55a24e0699f415ef71ace5b30510c698b0e6e08
Gavilan12 <toro21190@yahoo.com>
2018-11-04 18:04:01 -0600

    Script2

--- script1.sh
+++ script1.sh
@@ -3,7 +3,7 @@ mkdir dir2
 touch file1
 touch dirl/file10
 touch dir2/file20
-ln -T dir2/file20 link1
+ln -s dir2/file20 link1
 
 
 
---
+++ script2.sh
@@ -0,0 +1,14 @@
+ls
+
+touch script3.sh
+cat > script3.sh <<EOM
+
+rm file1 
+rm link1 
+rm -rf dir1
+rm -rf dir2
+
+EOM
+chmod 744 script3.sh
+
+ls
