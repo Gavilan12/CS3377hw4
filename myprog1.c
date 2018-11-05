2c5ea664d59a676177e9548e70a572873f572896
Gavilan12 <toro21190@yahoo.com>
2018-11-04 20:40:41 -0600

    part 4.3 done and tested

---
+++ myprog1.c
@@ -0,0 +1,11 @@
+  
+int main(){ 
+system("mkdir dir1");
+system("touch dir1/file10");
+system("mkdir dir2");
+system("touch dir1/file20");
+system("touch file1");
+system("ln -s dir2/file20 link1");
+return 0;
+}
+
